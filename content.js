9c9f3dc5d9b78ac30dc2ea70c12de1152a286927
Brad Czerniak <ao5357@gmail.com>
2014-01-22 12:44:37 -0500

    Fix selector typo

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  var link = document.querySelector("link[rel~='icon'], link[rel~='ICON'");
+  var link = document.querySelector("link[rel~='icon'], link[rel~='ICON']");
   if (!link) {
     link = document.createElement("link");
     link.setAttribute("rel", "icon");
